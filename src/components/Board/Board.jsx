ccceecf2db490a440a86b4f10589edfc435dda5e
logic-fabric <lowic@logic-fabric.fr>
2021-09-12 16:08:22 +0200

    feature: BoardSquares are now clickable with their states controlled by Board

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -5,34 +5,54 @@ import "./Board.css";
 class BoardSquare extends React.Component {
   render() {
     return (
-      <button className="board-square">
-        <div className="board-square__content">X</div>
+      <button className="board-square" onClick={() => this.props.onClick()}>
+        <div className="board-square__content">{this.props.value}</div>
       </button>
     );
   }
 }
 
 export class Board extends React.Component {
-  renderSquare(i) {
-    return <BoardSquare />;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(squareIndex) {
+    const newSquaresState = [...this.state.squares];
+    newSquaresState[squareIndex] = "X";
+
+    this.setState({ squares: newSquaresState });
+  }
+
+  renderSquare(squareIndex) {
+    return (
+      <BoardSquare
+        value={this.state.squares[squareIndex]}
+        onClick={() => this.handleClick(squareIndex)}
+      />
+    );
   }
 
   render() {
     return (
       <div className="board">
-        <div className="board-row">
+        <div className="board__row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
 
-        <div className="board-row">
+        <div className="board__row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
 
-        <div className="board-row">
+        <div className="board__row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
